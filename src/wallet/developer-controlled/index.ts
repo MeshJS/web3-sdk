d22fe8ceb004e54959610110ae685aab4319a482
Jingles <jinglescode@gmail.com>
2025-03-18 11:37:02 +0800

    Create index.ts

---
+++ src/wallet/developer-controlled/index.ts
@@ -0,0 +1,49 @@
+import { MeshWallet } from "@meshsdk/wallet";
+import { decryptStringWindow } from "../../functions";
+
+export default class DeveloperControlledWallets {
+  readonly privateKey: string;
+  readonly projectId: string;
+
+  constructor({
+    privateKey,
+    projectId,
+  }: {
+    privateKey: string;
+    projectId: string;
+  }) {
+    {
+      this.privateKey = privateKey;
+      this.projectId = projectId;
+    }
+  }
+
+  async createWallet() {}
+
+  async getWallets() {}
+
+  async getWallet(id: string) {
+    // todo: get wallet from db
+    const _wallet = {
+      id: "a5b5114ae2a9fdc05448e52aa384223d3ec77a964e0257276edb9c18",
+      mnemonic: "encryptedMnemonic",
+      networkId: 0 as 0 | 1,
+    };
+
+    const mnemonic = await decryptStringWindow(
+      this.privateKey,
+      _wallet.mnemonic
+    );
+
+    const wallet = new MeshWallet({
+      networkId: _wallet.networkId,
+      key: {
+        type: "mnemonic",
+        words: mnemonic.split(" "),
+      },
+    });
+    await wallet.init();
+
+    return wallet;
+  }
+}
