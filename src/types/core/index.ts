dd0a4da66c826b710dd1da8b512bbf157ff5c3ea
leif <leif@elliottnet.com>
2025-03-19 13:03:54 +0800

    an example of how this call can look

---
+++ src/types/core/index.ts
@@ -0,0 +1,9 @@
+/** A type copy of /packages/core/ Web3ProjectWallet. */
+export type SDKWeb3ProjectWallet = {
+  id: string;
+  address: string;
+  mnemonic: string;
+  networkId: 0 | 1;
+  tag: string | null;
+  projectId: string;
+};
--- src/wallet/developer-controlled/index.ts
+++ src/wallet/developer-controlled/index.ts
@@ -1,28 +1,46 @@
 import { MeshWallet } from "@meshsdk/wallet";
 import { decryptData } from "../../functions";
+import { SDKWeb3ProjectWallet } from "../../types/core";
 
 export default class DeveloperControlledWallets {
   readonly privateKey: string;
   readonly projectId: string;
   readonly apiKey: string;
+  readonly appUrl: string;
 
   constructor({
     privateKey,
     projectId,
     apiKey,
+    appUrl,
   }: {
     privateKey: string;
     projectId: string;
     apiKey: string;
+    appUrl?: string;
   }) {
     {
       this.privateKey = privateKey;
       this.projectId = projectId;
       this.apiKey = apiKey;
+      this.appUrl = appUrl ? appUrl : "https://web3.meshjs.dev/";
     }
   }
 
-  async createWallet() {}
+  async createWallet() {
+    const newWallet: SDKWeb3ProjectWallet = {};
+
+    const res = await fetch(this.appUrl + "api/project-wallet", {
+      method: "POST",
+      headers: {
+        "x-api-key": this.apiKey,
+      },
+      body: JSON.stringify(newWallet),
+    });
+
+    const wallet = (await res.json()) as SDKWeb3ProjectWallet;
+    // Decide how to return the wallet.
+  }
 
   async getWallets() {}
 
