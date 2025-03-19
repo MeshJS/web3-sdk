bdb79bda5017d13d4c337a06e293abebe851e405
Jingles <jinglescode@gmail.com>
2025-03-19 14:11:06 +0800

    create wallet wip

--- src/functions/encryption.ts
+++ src/functions/encryption.ts
@@ -1,3 +1,67 @@
+import * as crypto from "crypto";
+
+const IV_LENGTH = 16;
+
+export function encryptWithCipher({
+  data,
+  key,
+  algorithm = "aes-256-ctr",
+  initializationVectorSize = IV_LENGTH,
+}: {
+  data: any;
+  key: string;
+  algorithm?: string;
+  initializationVectorSize?: number;
+}) {
+  const _key = crypto
+    .createHash("sha256")
+    .update(String(key))
+    .digest("base64")
+    .substr(0, 32);
+
+  const buffer = Buffer.from(data);
+
+  // Create an initialization vector
+  const iv = crypto.randomBytes(initializationVectorSize);
+  // Create a new cipher using the algorithm, key, and iv
+  const cipher = crypto.createCipheriv(algorithm, _key, iv);
+  // Create the new (encrypted) buffer
+  const result = Buffer.concat([iv, cipher.update(buffer), cipher.final()]);
+
+  return result.toString("base64");
+}
+
+export function decryptWithCipher({
+  encryptedData,
+  key,
+  algorithm = "aes-256-ctr",
+  initializationVectorSize = 16,
+}: {
+  encryptedData: string;
+  key: string;
+  algorithm?: string;
+  initializationVectorSize?: number;
+}) {
+  key = crypto
+    .createHash("sha256")
+    .update(String(key))
+    .digest("base64")
+    .substr(0, 32);
+
+  let buffer = Buffer.from(encryptedData, "base64");
+
+  // Get the iv: the first 16 bytes
+  const iv = buffer.slice(0, initializationVectorSize);
+  // Get the rest
+  buffer = buffer.slice(initializationVectorSize);
+  // Create a decipher
+  const decipher = crypto.createDecipheriv(algorithm, key, iv);
+  // Actually decrypt it
+  const result = Buffer.concat([decipher.update(buffer), decipher.final()]);
+
+  return result.toString("utf8");
+}
+
 export async function generateKeyPair() {
   const keyPair = await window.crypto.subtle.generateKey(
     {
--- src/wallet/developer-controlled/index.ts
+++ src/wallet/developer-controlled/index.ts
@@ -1,22 +1,27 @@
 import { MeshWallet } from "@meshsdk/wallet";
-import { decryptData } from "../../functions";
+import { decryptData, encryptData } from "../../functions";
 import { SDKWeb3ProjectWallet } from "../../types/core";
+import { deserializeBech32Address } from "@meshsdk/core-cst";
 
-export default class DeveloperControlledWallets {
+export class DeveloperControlledWallets {
   readonly privateKey: string;
   readonly projectId: string;
   readonly apiKey: string;
   readonly appUrl: string;
+  readonly networkId: 0 | 1;
+  // project: Web3Project;
 
   constructor({
-    privateKey,
     projectId,
     apiKey,
+    networkId,
+    privateKey,
     appUrl,
   }: {
-    privateKey: string;
     projectId: string;
     apiKey: string;
+    networkId: 0 | 1;
+    privateKey: string;
     appUrl?: string;
   }) {
     {
@@ -24,11 +29,46 @@ export default class DeveloperControlledWallets {
       this.projectId = projectId;
       this.apiKey = apiKey;
       this.appUrl = appUrl ? appUrl : "https://web3.meshjs.dev/";
+      this.networkId = networkId;
     }
   }
 
-  async createWallet() {
-    const newWallet: SDKWeb3ProjectWallet = {};
+  async init() {
+    // this.project = await getProject(this.projectId);
+  }
+
+  async createWallet({ tag }: { tag?: string }) {
+    const mnemonic = MeshWallet.brew() as string[];
+    const _wallet = new MeshWallet({
+      networkId: this.networkId,
+      key: {
+        type: "mnemonic",
+        words: mnemonic,
+      },
+    });
+    await _wallet.init();
+
+    const encryptedMnemonic = await encryptData(
+      "this.project.publicKey", // get publicKey from project to encrypt
+      mnemonic.join(" ")
+    );
+
+    const addresses = await _wallet.getAddresses();
+    const address = addresses.baseAddressBech32!;
+    const keyHashesMainnet = deserializeBech32Address(address);
+
+    const pubKeyHash = keyHashesMainnet.pubKeyHash;
+
+    const newWallet: SDKWeb3ProjectWallet = {
+      id: pubKeyHash,
+      address: address,
+      mnemonic: encryptedMnemonic,
+      networkId: this.networkId,
+      tag: tag ? tag.trim() : null,
+      projectId: this.projectId,
+    };
+
+    console.log(44, newWallet);
 
     const res = await fetch(this.appUrl + "api/project-wallet", {
       method: "POST",
@@ -38,8 +78,9 @@ export default class DeveloperControlledWallets {
       body: JSON.stringify(newWallet),
     });
 
-    const wallet = (await res.json()) as SDKWeb3ProjectWallet;
-    // Decide how to return the wallet.
+    console.log("created wallet", res.status, res.json());
+
+    return _wallet;
   }
 
   async getWallets() {
@@ -51,7 +92,6 @@ export default class DeveloperControlledWallets {
         },
       }
     );
-
     const wallets: SDKWeb3ProjectWallet[] = await res.json();
   }
 
@@ -59,7 +99,13 @@ export default class DeveloperControlledWallets {
     // todo: get wallet from db
     const _wallet = {
       id: "a5b5114ae2a9fdc05448e52aa384223d3ec77a964e0257276edb9c18",
-      mnemonic: "encryptedMnemonic",
+      mnemonic: JSON.stringify({
+        ephemeralPublicKey:
+          "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEL2qsOtrwXrq7OJaaBSxiyO2cJ/OIcZ0Ndaq8goz7FwOURjXN3la2pWBX7A3rDWgDc3x2sZL6zy5JiM65o00YOQ==",
+        iv: "nHD5Udm3H166JI2k",
+        ciphertext:
+          "Du2aunbKxqdAfM69yKbb1Qs23NFIX/0RLRVjIZTSD6387Nhe3G8vAHqzaTCmmk4ljsrNa90GEq3l/jABfJXVYIBRpZmDOk+eT0nFhkLC+N3WPf2LS6R6ft2vFXha15CG55OOqLO5KG71+WdBFytk5+6dHXKEYz3e1VbQdhCd5QNbNkd8gfJ04b3gJI5yYNzPYkm/zqyA77dp3mix/Q+ZZ405NjYrjY7JPBLzEjXe",
+      }),
       networkId: 0 as 0 | 1,
     };
 
