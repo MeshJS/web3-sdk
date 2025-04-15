23cd5b8c256d66aee6f480b5c545b91486227260
SIDANWhatever <wongkahinhinson@gmail.com>
2025-04-15 16:48:07 +0800

    test: add decrypt IV12 test case

--- src/functions/crypto/encryption.test.ts
+++ src/functions/crypto/encryption.test.ts
@@ -12,11 +12,23 @@ const data =
 describe("with cipher", () => {
   const key = "01234567890123456789";
 
+  it("decrypt - 12 IV length", async () => {
+    const encryptedDataJSON =
+      '{"iv":"/bs1AzciZ1bDqT5W","ciphertext":"mh5pgH8ErqqH2KLLEBqqr8Pwm+mUuh9HhaAHslSD8ho6zk7mXccc9NUQAW8rb9UajCq8LYyANuiorjYD5N0hd2Lbe2n1x8AGRZrogyRKW6uhoFD1/FW6ofjgGP/kQRQSW2ZdJaDMbCxwYSdzxmaRunk6JRfybhfRU6kIxPMu41jhhRC3LbwZ+NnfBJFrg859hbuQgMQm8mqOUgOxcK8kKH54shOpGuLT4YBXhx33dZ//wT5VXrQ8kwIKttNk5h9MNKCacpRZSqU3pGlZ5oxucNEGos0IKTTXfbmwYx14uiERcXd32OP2"}';
+
+    const decrypted = await decryptWithCipher({
+      encryptedDataJSON: encryptedDataJSON,
+      key,
+    });
+
+    expect(data).toBe(decrypted);
+  });
   it("encrypt and decrypt", async () => {
     const encryptedDataJSON = await encryptWithCipher({
       data,
       key,
     });
+    console.log("encryptedDataJSON", encryptedDataJSON);
 
     const decrypted = await decryptWithCipher({
       encryptedDataJSON: encryptedDataJSON,
