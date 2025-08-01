25762b83d1f7a75e5e866ac3aef48da2a90148c6
unspent <205478529+unspent-tx@users.noreply.github.com>
2025-08-01 14:04:19 -0400

    export wallet https://github.com/MeshJS/web3-services/issues/258

--- src/types/window/index.ts
+++ src/types/window/index.ts
@@ -4,47 +4,64 @@ export * from "./wallet";
 import { Web3AuthProvider } from "../core";
 import { UserSocialData } from "../user";
 
-export type OpenWindowParams = {
-    method: "enable"
-    projectId: string;
-    directTo?: Web3AuthProvider
-    chain?: string;
-  } | {
-    method: "sign-tx"
-    projectId: string;
-    directTo?: Web3AuthProvider
-    unsignedTx: string;
-    partialSign: "true" | "false";
-    chain?: string;
-  } | {
-    method: "sign-data"
-    projectId: string
-    directTo?: Web3AuthProvider
-    payload: string;
-    address?: string;
-    chain?: string;
-    networkId?: string;
-  }
-
-export type OpenWindowResult = {
-    success: true
-    data: {
-        method: "enable"
-        cardanoPubKeyHash: string;
-        cardanoStakeCredentialHash: string;
-        bitcoinPubKeyHash: string;
-        user: UserSocialData
-    } | {
-        method: "sign-data"
-        signature: {
-            signature: string;
-            key: string;
-        };
-    } | {
-        method: "sign-tx"
-        tx: string;
+export type OpenWindowParams =
+  | {
+      method: "enable";
+      projectId: string;
+      directTo?: Web3AuthProvider;
+      chain?: string;
     }
-} | {
-    success: false;
-    message: string
-}
+  | {
+      method: "sign-tx";
+      projectId: string;
+      directTo?: Web3AuthProvider;
+      unsignedTx: string;
+      partialSign: "true" | "false";
+      chain?: string;
+    }
+  | {
+      method: "sign-data";
+      projectId: string;
+      directTo?: Web3AuthProvider;
+      payload: string;
+      address?: string;
+      chain?: string;
+      networkId?: string;
+    }
+  | {
+      method: "export-wallet";
+      projectId: string;
+      chain?: string;
+      networkId?: string;
+    };
+
+export type OpenWindowResult =
+  | {
+      success: true;
+      data:
+        | {
+            method: "enable";
+            cardanoPubKeyHash: string;
+            cardanoStakeCredentialHash: string;
+            bitcoinPubKeyHash: string;
+            user: UserSocialData;
+          }
+        | {
+            method: "sign-data";
+            signature: {
+              signature: string;
+              key: string;
+            };
+          }
+        | {
+            method: "sign-tx";
+            tx: string;
+          }
+        | {
+            method: "export-wallet";
+          };
+    }
+  | {
+      success: false;
+      message: string;
+    };
